2b4d7af153c8f69614eda66b06d07354dbc38d69
Rohan Singh <rohan-singh@hotmail.com>
2013-10-19 13:14:28 -0400

    Make users command work in steam rooms

--- SteamMobile/Commands/Users.cs
+++ SteamMobile/Commands/Users.cs
@@ -14,43 +14,50 @@ namespace SteamMobile.Commands
 
         public override void Handle(CommandTarget target, string[] parameters)
         {
-            if (!target.IsSession)
-                return;
-
-            var roomName = target.Session.Room;
-            var room = Program.RoomManager.Get(roomName);
-            if (room == null)
+            if (target.IsSession)
             {
-                target.Send("RohBot is not in the current chat.");
-                return;
+                var roomName = target.Session.Room;
+                var room = Program.RoomManager.Get(roomName);
+                if (room == null)
+                {
+                    target.Send("RohBot is not in the current chat.");
+                    return;
+                }
+
+                var steamRoom = room as SteamRoom;
+                var userList = new Packets.UserList();
+                var chat = Program.Steam.Status == Steam.ConnectionStatus.Connected && steamRoom != null ? steamRoom.Chat : null;
+                var steamUsers = chat != null ? chat.Members.ToList() : new List<SteamID>();
+                var sessions = Program.SessionManager.List.Where(s => s.Account != null).ToList();
+
+                foreach (var id in steamUsers.Where(i => i != Program.Steam.Bot.PersonaId))
+                {
+                    var persona = Program.Steam.Bot.GetPersona(id);
+                    var steamId = id.ConvertToUInt64().ToString("D");
+                    var groupMember = chat.Group.Members.FirstOrDefault(m => m.Id == id);
+                    var rank = groupMember != null ? groupMember.Rank.ToString() : "Member";
+                    var avatar = BitConverter.ToString(persona.Avatar).Replace("-", "").ToLower();
+                    userList.AddUser(persona.Name, steamId, rank, avatar, persona.PlayingName, false);
+                }
+
+                var accounts = sessions.Where(s => s.Room == roomName).Select(s => s.Account).Distinct(new Account.Comparer());
+
+                foreach (var account in accounts)
+                {
+                    var userId = account.Name.GetHashCode().ToString("D");
+                    userList.AddUser(account.Name, userId, "Member", "", "", true);
+                }
+
+                userList.Users = userList.Users.OrderBy(u => u.Name).ToList();
+                target.Session.Send(userList);
             }
-
-            var steamRoom = room as SteamRoom;
-            var userList = new Packets.UserList();
-            var chat = Program.Steam.Status == Steam.ConnectionStatus.Connected && steamRoom != null ? steamRoom.Chat : null;
-            var steamUsers = chat != null ? chat.Members.ToList() : new List<SteamID>();
-            var sessions = Program.SessionManager.List.Where(s => s.Account != null).ToList();
-
-            foreach (var id in steamUsers.Where(i => i != Program.Steam.Bot.PersonaId))
+            else if (target.IsRoom)
             {
-                var persona = Program.Steam.Bot.GetPersona(id);
-                var steamId = id.ConvertToUInt64().ToString("D");
-                var groupMember = chat.Group.Members.FirstOrDefault(m => m.Id == id);
-                var rank = groupMember != null ? groupMember.Rank.ToString() : "Member";
-                var avatar = BitConverter.ToString(persona.Avatar).Replace("-", "").ToLower();
-                userList.AddUser(persona.Name, steamId, rank, avatar, persona.PlayingName, false);
+                var roomName = target.Room.RoomInfo.ShortName;
+                var sessions = Program.SessionManager.List.Where(s => s.Account != null).ToList();
+                var accounts = sessions.Where(s => s.Room == roomName).Select(s => s.Account).Distinct(new Account.Comparer());
+                target.Send("In this chat: " + string.Join(", ", accounts.Select(a => a.Name)));
             }
-
-            var accounts = sessions.Where(s => s.Room == roomName).Select(s => s.Account).Distinct(new Account.Comparer());
-
-            foreach (var account in accounts)
-            {
-                var userId = account.Name.GetHashCode().ToString("D");
-                userList.AddUser(account.Name, userId, "Member", "", "", true);
-            }
-
-            userList.Users = userList.Users.OrderBy(u => u.Name).ToList();
-            target.Session.Send(userList);
         }
     }
 }
