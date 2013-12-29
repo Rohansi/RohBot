c23f5a50d3c0804d1743bd2e640d04cf9f3fe820
krixalis <krixdev@gmail.com>
2013-12-29 23:01:26 +0100

    Redundant hug command to show some love! <3
    Redundancy is beautiful when it involves love.

---
+++ SteamMobile/Commands/Hug.cs
@@ -0,0 +1,33 @@
+﻿namespace SteamMobile.Commands
+{
+    public class Hug : Command
+    {
+        public override string Type { get { return "hug"; } }
+
+        public override string Format(CommandTarget target, string type) { return "]"; }
+
+        public override void Handle(CommandTarget target, string type, string[] parameters)
+        {
+            if (!target.IsSession || target.Session.Account == null || parameters.Length == 0)
+                return;
+
+            if (Program.DelayManager.AddAndCheck(target.Session, 1))
+                return;
+
+            var room = Program.RoomManager.Get(target.Session.Room);
+            if (room == null)
+            {
+                target.Send("RohBot is not in this room.");
+                return;
+            }
+
+            if (room.IsBanned(target.Session.Account.Name))
+            {
+                target.Send("You are banned from this room.");
+                return;
+            }
+
+            room.Send(string.Format("{0} hugs {1}.", target.Session.Account.Name, parameters[0]));
+        }
+    }
+}
