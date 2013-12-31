d35e737767b30f98f9220591e015f026f5833e61
Lex Robinson <lexi@lexi.org.uk>
2013-12-31 12:10:36 +0000

    Use built in browser methods of submitting the login form

--- Web/js/rohbot.js
+++ Web/js/rohbot.js
@@ -1,12 +1,4 @@
 /* jshint jquery: true */
 $(document).ready(function() { 'use strict';
 	window.initializeRohBot();
-
-	$("#password").keydown(function(e) {
-		if (e.keyCode === 13) {
-			$("#password").blur().focus();
-			$("#loginButton").click();
-			return false;
-		}
-	});
 });
