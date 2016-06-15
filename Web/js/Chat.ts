a5fa1cdbf53a11e32421e7c14e0191fa7202b6a1
Rohan Singh <rohan-singh@hotmail.com>
2016-06-14 22:25:18 -0400

    Use noreferrer for links

--- Web/js/Chat.ts
+++ Web/js/Chat.ts
@@ -422,7 +422,7 @@ class Chat {
     private linkify(text: string) {
         // Put spaces infront of emoticons to terminate urls
         text = text.replace(/ː(\w+?)ː/g, " ː<img src=\"/economy/emoticon/$1\" alt=\":$1:\" class=\"emote\" title=\"$1\">");
-        text = text.replace(/((?:https?|ftps?|steam):\/\/\S+)/gi, "<a href=\"$1\" target=\"_blank\">$1</a>");
+        text = text.replace(/((?:https?|ftps?|steam):\/\/\S+)/gi, "<a href=\"$1\" rel=\"noreferrer\" target=\"_blank\">$1</a>");
         text = text.replace(/(^|[\s,;"'])([A-Za-z0-9\-.:]+?\.(?:com|net|org|uk|edu|gov|biz|me|ch|tv|io|ca|nz)(?:[\/:]\S*|\b))/gi, "$1<a href=\"http://$2\" target=\"_blank\">$2</a>");
         text = text.replace("\n", "<br>");
         text = text.replace(/\ ː/g, ""); // Get rid of the sentinel chars. (triangle colons are guaranteed to never appear in normal text)
