279438b7b3d62d20303d1729f8fd87b19710ab48
Brandon Bielsa <bgb7@njit.edu>
2016-07-02 11:54:46 -0400

    Added notification subscription packets

---
+++ RohBot/Packets/NotificationSubscription.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RohBot.Packets
+{
+    public class NotificationSubscription : Packet
+    {
+        public override string Type => "notificationSubscription";
+
+        public List<string> NotificationKeywords;
+
+        public override void Handle(Connection connection)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ RohBot/Packets/NotificationSubscriptionRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RohBot.Packets
+{
+    public class NotificationSubscriptionRequest : Packet
+    {
+        public override string Type => "notificationSubscriptionRequest";
+
+        public ICollection<String> Keywords;
+
+        public override void Handle(Connection connection)
+        {
+            Console.WriteLine("Received notificationSubscriptionRequest from {0}", connection.Session.Account.Name);
+
+            //throw new NotImplementedException();
+        }
+    }
+}
